0d9db304c024c7a09162edd6f03fa9f837a9645c
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 16:19:53 +0200

    Add project files.

---
+++ Lab1ex4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa semnul unui numar citit de la tastatura
+                • Daca numarul este pozitiv, va afisa “+”
+                • Daca numarul este negativ, va afisa “-”
+                • Daca numarul este 0, va afisa “0” */
+
+            Console.WriteLine("Introduceti un numar");
+            int numar = int.Parse(Console.ReadLine());
+
+            if (numar > 0)
+            {
+                Console.WriteLine("+");
+            }
+
+            if (numar < 0)
+            {
+                Console.WriteLine("-");
+            }
+            
+            if(numar==0)
+            {
+                Console.WriteLine("0");
+            }
+         
+
+
+        }
+    }
+}
